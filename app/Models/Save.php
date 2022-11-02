96e14978071743df9ad5934bcc50a518eab2c838
PimMiii <pim.milt@gmail.com>
2022-11-02 12:03:35 +0100

    Created Models

---
+++ app/Models/Save.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Save extends Model
+{
+    use HasFactory;
+}
