99960dbff91ba49230f96003d69ed21ad7fec036
Wessel <1023694@hr.nl>
2022-11-07 11:33:40 +0100

    created route for scenario

--- app/Models/Scenario.php
+++ app/Models/Scenario.php
@@ -4,17 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Scenario extends Model
 {
     use HasFactory;
+    protected $table = 'scenarios';
+    protected $primaryKey = 'id';
+    protected $fillable = ['dialogue', 'choice_id'];
 
-    public function story()
+    public function story(): BelongsTo
     {
         return $this->belongsTo(Story::class);
     }
 
-    public function choices()
+    public function choices(): HasMany
     {
         return $this->hasMany(Choice::class);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ScenarioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('scenarios', ScenarioController::class);
