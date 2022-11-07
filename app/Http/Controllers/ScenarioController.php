4c529aa4cd380caea2506d371a36bf415286a705
Wessel <1023694@hr.nl>
2022-11-07 11:33:16 +0100

    Made scenariocontroller with resource

--- app/Http/Controllers/ScenarioController.php
+++ app/Http/Controllers/ScenarioController.php
@@ -2,9 +2,98 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Scenario;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Redirector;
 
 class ScenarioController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function index()
+    {
+        $scenarios = Scenario::all();
+
+        return view('scenarios.index', compact('scenarios'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Application|Redirector|RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'dialogue' => 'required'
+        ]);
+
+        Scenario::create($data);
+        return redirect(route('scenarios.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit(Scenario $scenario)
+    {
+        $scenarios = Scenario::all();
+
+        return view('scenarios.edit', compact('scenarios'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return Application|RedirectResponse|Redirector
+     */
+    public function update(Request $request, Scenario $scenario)
+    {
+        $request->validate([
+            'dialogue' => 'required'
+        ]);
+
+        $scenario->update($request->all());
+        return redirect(route('scenarios.show'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
