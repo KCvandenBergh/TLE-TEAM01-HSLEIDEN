00e77741d0cd7542b6f9c43d42c436ed77cd5e7d
KCvandenBergh <89838896+KCvandenBergh@users.noreply.github.com>
2022-11-14 14:18:49 +0100

    als je dashboard in de url typt krijg je een melding dat je eerst moet inloggen.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'alreadyLoggedIn' => \App\Http\Middleware\AlreadyLoggedIn::class,
         'isLoggedIn' => \App\Http\Middleware\AuthCheck::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ use App\Http\Controllers\CustomAuthController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/login',[CustomAuthController::class,'login']);
-Route::get('/registration',[CustomAuthController::class,'registration']);
+Route::get('/login',[CustomAuthController::class,'login'])->middleware('alreadyLoggedIn');
+Route::get('/registration',[CustomAuthController::class,'registration'])->middleware('alreadyLoggedIn');
 Route::post('/register-user', [CustomAuthController::class,'registerUser'])->name('register-user');
 Route::post('login-user',[CustomAuthController::class,'loginUser'])->name('login-user');
 Route::get('/dashboard',[CustomAuthController::class,'dashboard'])->middleware('isLoggedIn');
