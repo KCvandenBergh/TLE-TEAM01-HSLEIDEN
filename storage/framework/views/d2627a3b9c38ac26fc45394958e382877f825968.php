9df8dad2244496a9682c8c3fb13c136559e3c75d
Pim <30662877+PimMiii@users.noreply.github.com>
2022-11-18 13:44:42 +0100

    Fixing .gitignore issues, and cleaning files that shouldve been ignored

--- storage/framework/views/d2627a3b9c38ac26fc45394958e382877f825968.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-
-    <h1><?php echo e($story->title); ?></h1>
-    <h2><?php echo e($story->id); ?></h2>
-    <p><?php echo e($story->description); ?></p>
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\TLE-TEAM01-HSLEIDEN\resources\views/cases/show.blade.php ENDPATH**/ ?>
