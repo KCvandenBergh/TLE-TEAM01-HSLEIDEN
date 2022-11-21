8bb0e938b8214dbb97cde4e649bf97979e234c06
wesselvbbb <1027694@hr.nl>
2022-11-21 15:02:25 +0100

    stories styling

--- resources/views/stories/index.blade.php
+++ resources/views/stories/index.blade.php
@@ -2,22 +2,34 @@
 @section('content')
 
     <div class="container">
-        <table class="table">
-            <tr>
-                <th>Title</th>
-                <th>Description</th>
-                <th></th>
-            </tr>
-            @foreach($stories as $story)
-                <tr>
-                    <td>{{$story->title}}</td>
-                    <td>{{$story->description}}</td>
-                    <td>
-                        <a href="{{route('stories.show',$story->id)}}">View</a>
-                    </td>
-                </tr>
+        <div id="searchBar">
+            <input type="text" placeholder="Search..">
+        </div>
+        @foreach($stories as $story)
+            <div class="card">
 
-            @endforeach
-        </table>
+                <h1>{{$story->title}}</h1>
+                <p>{{$story->description}}</p>
+                <a href="{{route('stories.show',$story->id)}}" class="play">Speel</a>
+            </div>
+        @endforeach
+
+        {{--        <table class="table">--}}
+        {{--            <tr>--}}
+        {{--                <th>Title</th>--}}
+        {{--                <th>Description</th>--}}
+        {{--                <th></th>--}}
+        {{--            </tr>--}}
+        {{--            @foreach($stories as $story)--}}
+        {{--                <tr>--}}
+        {{--                    <td>{{$story->title}}</td>--}}
+        {{--                    <td>{{$story->description}}</td>--}}
+        {{--                    <td>--}}
+        {{--                        <a href="{{route('stories.show',$story->id)}}" class="choices">View</a>--}}
+        {{--                    </td>--}}
+        {{--                </tr>--}}
+
+        {{--            @endforeach--}}
+        {{--        </table>--}}
     </div>
 @endsection
