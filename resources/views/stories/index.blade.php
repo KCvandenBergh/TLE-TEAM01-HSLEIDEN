add3221abff708938c477f28b012666cc6e91062
KCvandenBergh <89838896+KCvandenBergh@users.noreply.github.com>
2022-11-27 13:44:49 +0100

    - "clear" en "search" buttons gemaakt. - Zoekveld toegevoegd voor een keyword. - Select category keuzeknop aangemaakt.

--- resources/views/stories/index.blade.php
+++ resources/views/stories/index.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.app')
 @section('content')
 
+    <form class="form-check-inline" action="">
+        <label for="category_filter">Filter &nbsp;</label>
+        <select class="form-control" id="category_filter" name="category">
+            <option value="">Select Category</option>
+        </select>
+        <label for="keyword">&nbsp;&nbsp;</label>
+        <input type="text" class="form-control" name="keyword" placeholder="enter keyword" id="keyword">
+       <span>&nbsp;&nbsp;</span>
+
+        <button type="button" class="btn btn-primary">Search</button>
+        <a class="btn btn-success" href="">Clear</a>
+    </form>
+
+
     <div class="container">
         <table class="table">
             <tr>
@@ -20,4 +34,7 @@
             @endforeach
         </table>
     </div>
+
+
+
 @endsection
