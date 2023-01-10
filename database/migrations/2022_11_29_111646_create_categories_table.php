7ec3a6959274f75a8f9c61f3463307cffc40d458
KCvandenBergh <89838896+KCvandenBergh@users.noreply.github.com>
2023-01-10 14:43:13 +0100

    seeder succesvol

--- database/migrations/2022_11_29_111646_create_categories_table.php
+++ database/migrations/2022_11_29_111646_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('thema')->unique();
+            $table->string('name')->unique();
             $table->string('slug')->unique();
         });
     }
