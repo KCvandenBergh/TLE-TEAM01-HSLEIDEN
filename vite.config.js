bb961fecd5491da6ba762b444828a11650c0ff6b
Pim <30662877+PimMiii@users.noreply.github.com>
2023-01-13 11:32:59 +0100

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/js/timer.js'
             ],
             refresh: true,
         }),
